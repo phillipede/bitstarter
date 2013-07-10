48232162c41664737b82d94acb680677ece1a5db
Phillip Monell <phillipmonell@gmail.com>
2013-07-10 17:26:23 +0000

    Buffer debugging

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
-var string = fs.readFileSync('index.html');
+var string = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString(string));
